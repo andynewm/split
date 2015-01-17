237d68fe5dd4e14d3adc6c8376325d8219225db2
Andy Newman <anxx88@gmail.com>
2015-01-17 16:40:32 +0000

    t

--- make/make.js
+++ make/make.js
@@ -1,8 +1,12 @@
+var poly = [];
+
 (function () {
     var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
 
-    var poly = [], mx, my;
+    context.fillStyle = 'rgba(255,128,0,0.3)';
+
+    var mx, my;
 
     $(canvas).on('click', function (event) {
         poly.push([
@@ -12,6 +16,9 @@
     }).on('mousemove', function (event) {
         mx = event.offsetX;
         my = event.offsetY;
+    }).on('mouseleave', function () {
+        mx = null;
+        my = null;
     });
 
     (function draw() {
@@ -20,13 +27,20 @@
         if (poly.length) {
             context.beginPath();
 
-            context.moveTo(mx, my);
+            context.moveTo(poly[0][0], poly[0][1]);
 
-            poly.forEach(function (p) {
+            poly.slice(1).forEach(function (p) {
                 context.lineTo(p[0], p[1]);
             });
 
-            context.closePath();
+            if (mx) {
+                context.lineTo(mx, my);
+            }
+            else {
+                context.closePath();
+            }
+
+            context.fill();
 
             context.stroke();
         }
@@ -36,11 +50,29 @@
 
     $('#save').on('click', function () {
         $.ajax('/polygon', {
-            data: JSON.stringify({poly: poly}),
+            data: JSON.stringify({points: poly}),
             type: 'POST',
             contentType: 'application/json'
         });
-    })
+    });
+
+    $.get('/polygon')
+        .then(function (n) {
+            var i;
+            for (i = 1; i <= n; i++) {
+                (function (i) {
+                    $('<a>')
+                        .text('Number: ' + i)
+                        .on('click', function () {
+                            $.get('/polygon/' + i)
+                                .then(function (p) {
+                                    poly = p.points;
+                                });
+                        })
+                        .appendTo('#puzzles');
+                })(i);
+            }
+        });
 
 
 })();
